71623422706df0eb7287532bfca7d549895aae4a
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-04-26 15:20:38 -0500

    Create cc.py

--- cc.py
+++ cc.py
@@ -13,7 +13,6 @@ from os import system
 # SUPER GEN CC
 # CREATED BY. @DEMO 593
 
-system("chmod 777 cc.py")
 system("clear")
 
 def detect_os():
@@ -41,7 +40,7 @@ print (bl + "[-] Powered by Scream Bins ")
 print (gn + "[+] Made By Community")
 print (cy + "[=] Generador CC : 1.5")
 
-opr = input (mag + "\n\033[1;31m1\033[1;34m) \033[1;35mGenerar tarjeta Australia (PRIME VIDEO,CRUNCHYROLL)\n\033[1;31m2\033[1;34m) \033[1;35mGenerar tarjeta Canada (PRIME VIDEO,SPOTIFY,CANVA,DEEZER)\n\033[1;31m3\033[1;34m) \033[1;35mGenerar tarjeta Oman (LIONSGATE+)\n\033[1;31m4\033[1;34m) \033[1;35mGenerar tarjeta Uruguay (SPOTIFY) \n\033[1;31m5\033[1;34m) \033[1;35mGenerar tarjeta United Kingdom (ONLYFANS)\n\033[1;31m6\033[1;34m) \033[1;35mGenerar tarjeta Argentina (RAPPI)\n\033[1;31m7\033[1;34m) \033[1;35mGenerar tarjeta Ecuador (CRUNCHYROLL,HBO MAX)\n\033[1;31m8\033[1;34m) \033[1;35mGenerar tarjeta PANAMA (PARAMOUNT+)\n\033[1;31m9\033[1;34m) \033[1;35m Generar tarjeta Belgium (CANVA,YOUTUBE PREMIUN,TIDAL)\n\033[1;31m10\033[1;34m) \033[1;35mGenerar tarjeta Mexico (PARAMOUNT+,ACORN TV,TIDAL,CANVA)\n\033[1;31m11\033[1;34m) \033[1;35mGenerar tarjeta Colombia (SPOTIFY,RAPPI COLOMBIA,OTRO)\n\033[1;31m12\033[1;34m) \033[1;35mGenerar tarjeta Turkia (SPOTIFY)\n\033[1;31m13\033[1;34m) \033[1;35mGenerar tarjeta EEUU (DISNEY,YOUTUBE,NORD VPN,SPOTIFY)\n\n\033[1;32m[^] Opción :  ")
+opr = input (mag + "\n\033[1;31m1\033[1;34m) \033[1;35mGenerar tarjeta Australia (PRIME VIDEO,CRUNCHYROLL)\n\033[1;31m2\033[1;34m) \033[1;35mGenerar tarjeta Canada (PRIME VIDEO,SPOTIFY,CANVA,DEEZER)\n\033[1;31m3\033[1;34m) \033[1;35mGenerar tarjeta Oman (LIONSGATE+)\n\033[1;31m4\033[1;34m) \033[1;35mGenerar tarjeta Uruguay (SPOTIFY) \n\033[1;31m5\033[1;34m) \033[1;35mGenerar tarjeta United Kingdom (ONLYFANS)\n\033[1;31m6\033[1;34m) \033[1;35mGenerar tarjeta Argentina (RAPPI)\n\033[1;31m7\033[1;34m) \033[1;35mGenerar tarjeta Ecuador (CRUNCHYROLL,HBO MAX)\n\033[1;31m8\033[1;34m) \033[1;35mGenerar tarjeta PANAMA (PARAMOUNT+)\n\033[1;31m9\033[1;34m) \033[1;35m Generar tarjeta Belgium (CANVA,YOUTUBE PREMIUN)\n\033[1;31m10\033[1;34m) \033[1;35mGenerar tarjeta Mexico (PARAMOUNT+,ACORN TV,TIDAL,CANVA)\n\033[1;31m11\033[1;34m) \033[1;37mGenerar tarjeta Colombia (SPOTIFY,RAPPI COLOMBIA,OTRO)\n\033[1;31m12\033[1;34m) \033[1;37mGenerar tarjeta Turquía (SPOTIFY)\n\033[1;31m13\033[1;34m) \033[1;35mGenerar tarjeta EEUU (DISNEY,YOUTUBE,NORD VPN,SPOTIFY)\n\n\033[1;32m[^] Opción :  ")
 
 def genscard():
     cookies = {"csrftoken":"8b56rI96TwUH0X7dOT86JmPMBbUVYEpX3EI7ZKp3ZXHWnrRySD9ORyNaAaRXnW7i","_ga":"GA1.2.1579916434.1654760883","_gid":"GA1.2.1410860416.1654760883","_gads":"ID=d4f0fe2265535514-2243e178fad30069:T=1654760893:RT=1654760893:S=ALNI_MaIzJo5Kmg3rKoLXSuvDGnQkyW3uw","_gpi":"UID=0000087f297f7f43:T=1654760893:RT=1654760893:S=ALNI_MbnajBnRWmSHW7vrpR-U1w2uMwyVw",'FCNEC':'[["AKsRol_6etCde6kaPNd_o13SF2anvKLy0qaXvN6Kz0O_d9YbYS_KOfZ-j0xDjsEXL_4Otx5R38juHOOwfg0JShy5DHGmgAw2R6ZN4KZyI3qGimMjR0mQ0SEgj2ncvV4jQ32pssYst9ml2ptS_Ip2XyPbrLivgKXjIQ=="],null,[]]'}
@@ -151,7 +150,7 @@ def colombia():
     rs = requests.post(sitex , headers=headers , cookies=cookies,data=payload)
     data = json.loads(rs.text)
     card = data['creditCard'][1]
-    return ("\033[1;32m[-] TIPO : %s\n[-] Numero de Tarjeta : %s\n[-] Banco : %s\n[-] Pais : %s\n[-] CVV : %s\n[-] Expira : %s\n============================\n[*] Telegram : @Demo593" % (card['IssuingNetwork'] , card['CardNumber'] , card['Bank'] , card['Country'] , card['CVV'] , card['Expiry']) + cv)
+    return ("\033[1;32m[-] TIPO : %s\n[-] Numero de Tarjeta : %s\n[-] Banco : %s\n[-] Pais : %s\n[-] CVV : %s\n[-] Expira : %s\n============================\n[*] Grupo WhatsApp : https://chat.whatsapp.com/BQQ9ARePQEJGz99DFz8CN8" % (card['IssuingNetwork'] , card['CardNumber'] , card['Bank'] , card['Country'] , card['CVV'] , card['Expiry']) + cv)
 
 def turkia():
     cookies = {"csrftoken":"8b56rI96TwUH0X7dOT86JmPMBbUVYEpX3EI7ZKp3ZXHWnrRySD9ORyNaAaRXnW7i","_ga":"GA1.2.1579916434.1654760883","_gid":"GA1.2.1410860416.1654760883","_gads":"ID=d4f0fe2265535514-2243e178fad30069:T=1654760893:RT=1654760893:S=ALNI_MaIzJo5Kmg3rKoLXSuvDGnQkyW3uw","_gpi":"UID=0000087f297f7f43:T=1654760893:RT=1654760893:S=ALNI_MbnajBnRWmSHW7vrpR-U1w2uMwyVw",'FCNEC':'[["AKsRol_6etCde6kaPNd_o13SF2anvKLy0qaXvN6Kz0O_d9YbYS_KOfZ-j0xDjsEXL_4Otx5R38juHOOwfg0JShy5DHGmgAw2R6ZN4KZyI3qGimMjR0mQ0SEgj2ncvV4jQ32pssYst9ml2ptS_Ip2XyPbrLivgKXjIQ=="],null,[]]'}
@@ -161,7 +160,7 @@ def turkia():
     rs = requests.post(sitex , headers=headers , cookies=cookies,data=payload)
     data = json.loads(rs.text)
     card = data['creditCard'][1]
-    return ("\033[1;32m[-] TIPO : %s\n[-] Numero de Tarjeta : %s\n[-] Banco : %s\n[-] Pais : %s\n[-] CVV : %s\n[-] Expira : %s\n============================\n[*] Telegram : @Demo593" % (card['IssuingNetwork'] , card['CardNumber'] , card['Bank'] , card['Country'] , card['CVV'] , card['Expiry']) + cv)
+    return ("\033[1;32m[-] TIPO : %s\n[-] Numero de Tarjeta : %s\n[-] Banco : %s\n[-] Pais : %s\n[-] CVV : %s\n[-] Expira : %s\n============================\n[*] Grupo WhatsApp : https://chat.whatsapp.com/BQQ9ARePQEJGz99DFz8CN8" % (card['IssuingNetwork'] , card['CardNumber'] , card['Bank'] , card['Country'] , card['CVV'] , card['Expiry']) + cv)
 
 def estadosu():
     cookies = {"csrftoken":"8b56rI96TwUH0X7dOT86JmPMBbUVYEpX3EI7ZKp3ZXHWnrRySD9ORyNaAaRXnW7i","_ga":"GA1.2.1579916434.1654760883","_gid":"GA1.2.1410860416.1654760883","_gads":"ID=d4f0fe2265535514-2243e178fad30069:T=1654760893:RT=1654760893:S=ALNI_MaIzJo5Kmg3rKoLXSuvDGnQkyW3uw","_gpi":"UID=0000087f297f7f43:T=1654760893:RT=1654760893:S=ALNI_MbnajBnRWmSHW7vrpR-U1w2uMwyVw",'FCNEC':'[["AKsRol_6etCde6kaPNd_o13SF2anvKLy0qaXvN6Kz0O_d9YbYS_KOfZ-j0xDjsEXL_4Otx5R38juHOOwfg0JShy5DHGmgAw2R6ZN4KZyI3qGimMjR0mQ0SEgj2ncvV4jQ32pssYst9ml2ptS_Ip2XyPbrLivgKXjIQ=="],null,[]]'}
